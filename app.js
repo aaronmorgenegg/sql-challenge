bf562885c4eb626a3036fed11a28f1b089ac6fbe
aaronmorgenegg <aaronmorgenegg@gmail.com>
2016-11-21 13:16:37 -0700

    moved things around, installed more npm modules,

--- app.js
+++ app.js
@@ -1,11 +1,67 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var pgp = require('pg-promise')();
+var db = pgp('postgres://localhost:5432/generator');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.json())app.use( function( req, res, next ) {
+  if (req.query._method == 'DELETE') {
+    req.method = 'DELETE';
+    req.url = req.path;
+  }
+  next();
+});
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname);
 
-app.get('/', function (req, res) {
-  res.render('index');
+//get users
+app.get('/', function(req,res,next){
+  db.any('SELECT * FROM users')
+    .then(function(data){
+      return res.render('index', {data: data})
+    })
+    .catch(function(err){
+      return next(err);
+    });
+});
+
+//edit users
+app.get('/users/:id/edit', function(req,res,next){
+  var id = parseInt(req.params.id);
+  db.one('select * from users where id = $1', id)
+    .then(function(user){
+      res.render('edit', {user: user})
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+});
+
+app.post('/users/:id/edit', function(req, res, next){
+  db.none('update users set name = $1, email = $2, password = $s3 where id = $4',
+    [req.body.name, req.body.email, req.body.password, parseInt(req.params.id)])
+    .then(function() {
+      res.redirect('/');
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+});
+
+app.delete('/users/:id', function(req, res, next){
+  var id = parseInt(req.params.id);
+  db.results('delete from users where id = $1', id)
+    .then(function(result) {
+      res.redirect('/');
+    })
+    .catch(function(err) {
+      return next(err);
+    });
 });
 
 app.listen(3000, function () {
