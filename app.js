b11c86750123d31e33d25e3258a552caaf6dbeb1
aaronmorgenegg <aaronmorgenegg@gmail.com>
2016-11-20 19:29:44 -0700

    Copied over stuff from first milestone, started converting it to be used here

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname);
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!')
+});
